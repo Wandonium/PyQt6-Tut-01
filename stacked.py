368490a68acc36da46bee1127f9d6b3bf2a3660f
Wandonium <hillarywando@gmail.com>
2022-06-22 22:31:16 +0300

    Add second page to the Stacked Layout (Profile page).

--- stacked.py
+++ stacked.py
@@ -33,6 +33,26 @@ class MainWindow(QWidget):
         profile_image.setPixmap(pixmap)
         profile_image.setScaledContents(True)
 
+        # Create the Profile page (Page 2)
+        pg2_form = QFormLayout()
+        pg2_form.setFieldGrowthPolicy(
+            pg2_form.FieldGrowthPolicy.AllNonFixedFieldsGrow
+        )
+        pg2_form.setFormAlignment(
+            Qt.AlignmentFlag.AlignHCenter |
+            Qt.AlignmentFlag.AlignTop
+        )
+        pg2_form.setLabelAlignment(Qt.AlignmentFlag.AlignLeft)
+        pg2_form.addRow("Breed:", QLabel("Norwegian Forest Cat"))
+        pg2_form.addRow("Origin:", QLabel("Norway"))
+        pg2_form.addRow(QLabel("Description:"))
+        default_text = """Have a long, sturdy body, long legs and a bushy tail.
+        They are friendly, intelligent, and generally good with people."""
+        pg2_form.addRow(QTextEdit(default_text))
+
+        pg2_container = QWidget()
+        pg2_container.setLayout(pg2_form)
+
 # Run the program
 if __name__ == '__main__':
     app = QApplication(sys.argv)
