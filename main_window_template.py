964cad9b0bb3a0b5cf580b4ff1ef4ca85ec2befd
Wandonium <hillarywando@gmail.com>
2022-06-23 21:54:47 +0300

    Add methods for creating a menu bar in an application.

--- main_window_template.py
+++ main_window_template.py
@@ -19,6 +19,25 @@ class MainWindow(QMainWindow):
         self.createMenu()
         self.show()
 
+    def setUpMainWindow(self):
+        """Create and arrange widgets in the main window."""
+        pass
+
+    def createActions(self):
+        """Create the application's menu actions."""
+        # Create actions for File menu
+        self.quit_act = QAction("&Quit")
+        self.quit_act.setShortcut("Ctrl+Q")
+        self.quit_act.triggered.connect(self.close)
+
+    def createMenu(self):
+        """Create the application's menu bar."""
+        self.menuBar().setNativeMenuBar(False)
+
+        # Create file menu and add actions
+        file_menu = self.menuBar().addMenu("File")
+        file_menu.addAction(self.quit_act)
+
 # Run the program
 if __name__ == '__main__':
     app = QApplication(sys.argv)
