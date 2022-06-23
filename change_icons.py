4085ec243088d97fdc25e7e4e750a8dce97890eb
Wandonium <hillarywando@gmail.com>
2022-06-23 22:20:24 +0300

    Create the setUpMainWindow method for the change_icon program.

--- change_icons.py
+++ change_icons.py
@@ -20,6 +20,22 @@ class MainWindow(QMainWindow):
         self.setUpMainWindow()
         self.show()
 
+    def setUpMainWindow(self):
+        """Create and arrange widgets in the main window."""
+        info_label = QLabel("Click on the button and select a fruit.")
+        info_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.images = [
+            "images/1_apple.png",
+            "images/2_pineapple.png",
+            "images/3_watermelon.png",
+            "images/4_banana.png"
+        ]
+        self.icon_button = QPushButton()
+        self.icon_button.setIcon(QIcon(random.choice(self.images)))
+        self.icon_button.setIconSize(QSize(60, 60))
+        self.icon_button.clicked.connect(self.changeButtonIcon)
+
 # Run the program
 if __name__ == '__main__':
     app = QApplication(sys.argv)
