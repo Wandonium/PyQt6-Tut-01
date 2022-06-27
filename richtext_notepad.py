1fa9fdcbbfc4e42f145a09b60b97390dfe15ca6e
Wandonium <hillarywando@gmail.com>
2022-06-27 21:47:36 +0300

    Create the createActions() method for the File menu actions.

--- richtext_notepad.py
+++ richtext_notepad.py
@@ -23,12 +23,26 @@ class MainWindow(QMainWindow):
 
     def setUpMainWindow(self):
         """Create and arrange widgets in the main window."""
-        pass
+        self.text_edit = QTextEdit()
+        self.text_edit.textChanged.connect(self.removeHighlights)
+        self.setCentralWidget(self.text_edit)
 
     def createActions(self):
         """Create the application's menu actions."""
         # Create actions for File menu
-        self.quit_act = QAction("&Quit")
+        self.new_act = QAction(QIcon("images/new_file.png"), "New")
+        self.new_act.setShortcut("Ctrl+N")
+        self.new_act.triggered.connect(self.clearText)
+
+        self.open_act = QAction(QIcon("images/open_file.png"), "Open")
+        self.open_act.setShortcut("Ctrl+O")
+        self.open_act.triggered.connect(self.openFile)
+
+        self.save_act = QAction(QIcon("images/save_file.png"), "Save")
+        self.save_act.setShortcut("Ctrl+S")
+        self.save_act.triggered.connect(self.saveToFile)
+
+        self.quit_act = QAction(QIcon("images/exit.png"), "Quit")
         self.quit_act.setShortcut("Ctrl+Q")
         self.quit_act.triggered.connect(self.close)
 
@@ -38,7 +52,7 @@ class MainWindow(QMainWindow):
 
         # Create file menu and add actions
         file_menu = self.menuBar().addMenu("File")
-        file_menu.addAction(self.quit_act)
+        file_menu.addAction(self.new_act)
 
 # Run the program
 if __name__ == '__main__':
